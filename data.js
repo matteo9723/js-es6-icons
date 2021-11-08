785a0cbfc593c99a96562d8faf7db4245842f28a
matteo9723 <matteolandri7@gmail.com>
2021-11-08 17:29:57 +0100

    quasi finito manca il reset

--- data.js
+++ data.js
@@ -1,4 +1,4 @@
-[
+const arr = [
 	{
 		name: 'cat',
 		prefix: 'fa-',
@@ -113,4 +113,85 @@
 	}
 ];
 
-console.log('ciao');
+let main = document.getElementById('main');
+console.log(main);
+
+
+
+// main.innerHTML=`
+// <div class="card">
+// <i class="${arr[0].family} ${arr[0].prefix}${arr[0].name} ${arr[0].color}"></i>
+// <div class="text "> ${arr[0].name} </div>
+// </div>`;
+for(let i in arr){
+
+	main.innerHTML+=`
+	<div class="card">
+	<i class="${arr[i].family} ${arr[i].prefix}${arr[i].name} ${arr[i].color}"></i>
+	<div class="text "> ${arr[i].name} </div>
+	</div>`;
+}
+
+
+let select=document.getElementById('select');
+let enter = false;
+
+select.addEventListener('click',function(){
+	
+	let valoreSelect = document.getElementById("select").value;
+
+	// ANIMAL 
+	
+	if(valoreSelect == 'animal' && enter == false){
+		const selezione = arr.filter((x)=>{
+			enter=true;
+			return x.type == 'animal';
+		});
+		for(let i in selezione){
+
+			main.innerHTML+=`
+			<div class="card">
+			<i class="${selezione[i].family} ${selezione[i].prefix}${selezione[i].name} ${selezione[i].color}"></i>
+			<div class="text "> ${selezione[i].name} </div>
+			</div>`;
+		}
+	}
+
+	// VEGETABLE
+
+	if(valoreSelect == 'vegetable' && enter == false){
+		const selezione = arr.filter((x)=>{
+			enter=true;
+			return x.type == 'vegetable';
+		});
+		for(let i in selezione){
+
+			main.innerHTML+=`
+			<div class="card">
+			<i class="${selezione[i].family} ${selezione[i].prefix}${selezione[i].name} ${selezione[i].color}"></i>
+			<div class="text "> ${selezione[i].name} </div>
+			</div>`;
+		}
+	}
+
+	// USER 
+
+	if(valoreSelect == 'user' && enter == false){
+		const selezione = arr.filter((x)=>{
+			enter=true;
+			return x.type == 'user';
+		});
+		for(let i in selezione){
+
+			main.innerHTML+=`
+			<div class="card">
+			<i class="${selezione[i].family} ${selezione[i].prefix}${selezione[i].name} ${selezione[i].color}"></i>
+			<div class="text "> ${selezione[i].name} </div>
+			</div>`;
+		}
+	}
+
+});
+
+
+
